f4422e8552afcb6b8801b1ca51daab055ab33a35
fbslo <admin@fbslo.net>
2024-02-06 20:11:40 +0100

    start adding recovery

--- app/src/apdu_handler.c
+++ app/src/apdu_handler.c
@@ -44,7 +44,7 @@ void extractHDPath(uint32_t rx, uint32_t offset) {
     memcpy(hdPath, G_io_apdu_buffer + offset, sizeof(uint32_t) * HDPATH_LEN_DEFAULT);
 
     const bool mainnet = hdPath[0] == HDPATH_0_DEFAULT &&
-                         hdPath[1] == HDPATH_1_DEFAULT;
+                         hdPath[1] == HDPATH_1_RECOVERY;
 
     if (!mainnet) {
         THROW(APDU_CODE_DATA_INVALID);
